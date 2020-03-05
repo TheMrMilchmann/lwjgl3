b538b3e27691c54295abdaf79a0123a868e0f61d
Leon Linhart <themrmilchmann@gmail.com>
2020-03-05 01:22:29 +0100

    WIP: fixup

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@
  * Copyright LWJGL. All rights reserved.
  * License terms: https://www.lwjgl.org/license
  */
+plugins {
+    id("me.champeau.gradle.jmh") version "0.5.0" apply false
+}
+
 val lwjglVersion: String by project
 
 allprojects {
