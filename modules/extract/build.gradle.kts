ba4dfad03a9bbf36f491896f5c6f11c5bf2459ed
Leon Linhart <themrmilchmann@gmail.com>
2020-03-05 13:08:54 +0100

    WIP: configure Kotlin compilations

--- modules/extract/build.gradle.kts
+++ modules/extract/build.gradle.kts
@@ -11,6 +11,21 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf(
+                "-Xno-call-assertions",
+                "-Xno-param-assertions",
+                "-Xreport-perf",
+                "-progressive",
+                "-Xuse-experimental=kotlin.Experimental",
+                "-Xuse-experimental=kotlin.contracts.ExperimentalContracts"
+            )
+        }
+    }
+}
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":lwjgl.llvm"))
--- modules/generator/build.gradle.kts
+++ modules/generator/build.gradle.kts
@@ -2,6 +2,8 @@
  * Copyright LWJGL. All rights reserved.
  * License terms: https://www.lwjgl.org/license
  */
+import org.jetbrains.kotlin.gradle.tasks.*
+
 plugins {
     `java-library`
     kotlin("jvm")
@@ -10,6 +12,20 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf(
+                "-Xno-call-assertions",
+                "-Xno-param-assertions",
+                "-Xreport-perf",
+                "-progressive"
+            )
+        }
+    }
 }
 
 dependencies {
