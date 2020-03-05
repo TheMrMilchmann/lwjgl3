0d73aa860a589a062d392e51bef46ec25ae927da
Leon Linhart <themrmilchmann@gmail.com>
2020-03-05 01:16:05 +0100

    WIP: add TestNG dependency

--- modules/samples/build.gradle.kts
+++ modules/samples/build.gradle.kts
@@ -12,10 +12,17 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+tasks {
+    test {
+        useTestNG()
+    }
+}
+
 dependencies {
     rootProject.childProjects.values.filter { it.name.startsWith("lwjgl.") }.forEach { lwjglModule ->
         testImplementation(project(":${lwjglModule.name}"))
     }
     testImplementation(group = "org.joml", name = "joml", version = "1.9.22")
+    testImplementation(group = "org.testng", name = "testng", version = "7.0.0")
     testCompileOnly(group = "com.google.code.findbugs", name = "jsr305", version = "3.0.2")
 }
